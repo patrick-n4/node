269c5bbc65c962df96213787273ec48582c4ec41
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-04 20:02:43 +0200

    This is the backend for the sockets

--- socket/server/server.js
+++ socket/server/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io")
-const cors = require("cors")
-app.use(cors());
 const PORT = process.env.PORT || 7000;
-
+const cors = require("cors");
+const { Server } = require("socket.io");
 const server = http.createServer(app);
-
+app.use(cors());
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    method: ["POST","GET"]
-  }
-})
+  cors: "http://localhost:3000",
+  methods: ["POST", "GET"]
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    socket.join(data.room);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receiveMessage", data);
+  });
+  console.log(socket)
+});
 
 server.listen(PORT, () => {
-  console.log(`Server is running on localhost${PORT}`);
+  console.log(`Server is running on localhost:${PORT}`);
 });
