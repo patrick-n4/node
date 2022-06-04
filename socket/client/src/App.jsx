a64da6b676049b2fcfe6925af5e7e6774592c3d3
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-04 09:21:40 +0200

    This is the interface for sending the messages

--- socket/client/src/App.jsx
+++ socket/client/src/App.jsx
@@ -1,4 +1,22 @@
 import React from "react";
 export default function App() {
-  return (<div className="text-red-700">On the server</div>)
+  return (
+    <div className="h-[100vh] w-full bg-red-3001 flex items-center justify-center">
+      <div className="h-[50%] w-[40%] bg-cyan-50 shadow-2xl flex p-[1em] flex-col items-center justify-between">
+        <h1 className="text-[1.5em] font-bold ">Messages </h1>
+        <div className="h-[90%] w-full flex justify-between flex-row">
+          <div className="flex  justify-evenly w-full h-[20%]">
+            <input
+              type="text"
+              placeholder="Message...."
+              className="w-[50%] text-[1.4em] h-[80%] rounded-md shadow-lg"
+            />
+            <button className="bg-green-400 text-[1.4em] h-[80%] flex items-center justify-center w-[30%] rounded-lg shadow-lg">
+              Send Message
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- socket/server/server.js
+++ socket/server/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const PORT = process.env.PORT || 7000;
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello I am running on 7000");
-});
-app.listen(PORT, () => {
-  console.log(`Server running on localhost:${PORT}`);
+const http = require("http");
+const { Server } = require("socket.io")
+const cors = require("cors")
+app.use(cors());
+const PORT = process.env.PORT || 7000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["POST","GET"]
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on localhost${PORT}`);
 });
