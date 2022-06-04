1ae7104d1391b8889ef771a5eedb4931c5ccd1b4
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-04 21:35:49 +0200

    Continuing with the mern app

--- jwt/client/src/App.jsx
+++ jwt/client/src/App.jsx
@@ -1,3 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./Components/LOGIN/Login";
+import Signup from "./Components/SIGNUP/signup";
+import Home from "./Components/HOME/home";
 export default function App() {
-  return <div className="">Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home/:id" element={<Home />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
