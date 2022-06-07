d07278bac42921e1288447b2914791c03b6888bc
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-07 05:11:10 +0200

    This is renaming the folders that had a bad naming convention

--- jwt/server/routers/login.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const login = require("../controllers/handleLogin");
-const router = express.Router();
-
-router.route('/').post(login);
-
-module.exports = router;
