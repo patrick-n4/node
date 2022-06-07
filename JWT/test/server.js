54d58aca21e35fe84d94fe76ae685592df4d800a
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-07 07:37:58 +0200

    Mastered the way the tokens are working in behind the scenes

--- JWT/test/server.js
+++ JWT/test/server.js
@@ -5,36 +5,37 @@ require("dotenv").config();
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-
-//Making posts
 const posts = [
   { username: "Patrick", title: "Post 1" },
   { username: "Prince", title: "Post 2" }
 ];
+let refreshTokens = [];
 app.use(express.json());
-app.get("/posts", verifyToken, (req, res) => {
-  res.json(posts.filter((post) => post.username === req.user.name));
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) return res.send("Refresh token doesn't exist");
+  if (!refreshTokens.includes(refreshToken))
+    return res.send("Refresh token not found");
+  jwt.verify(refreshToken, process.env.ACCESS_REFRESH_TOKEN, (err, data) => {
+    if (err) res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: data.name });
+    res.json({ accessToken });
+  });
 });
-
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30s"});
-  res.send(token);
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.ACCESS_REFRESH_TOKEN);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
-
-function verifyToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader.split(" ")[1];
-  if (token == null) return res.send("Token Doesn't exists");
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
-    if (err) {
-      return res.send("Invalid Token");
-    } else {
-      req.user = data;
-      next();
-    }
-  });
+app.delete("/logout", (req, res) => {
+  refreshTokens = [];
+  res.send("Token deleted successfully");
+});
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30s" });
 }
 server.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
