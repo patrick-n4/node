7575dda28e078e0c1c30c2cb60b333180712b37a
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-07 06:26:22 +0200

    Generated an accessToken

--- JWT/test/server.js
+++ JWT/test/server.js
@@ -1,46 +1,41 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 7070;
+const PORT = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-app.use(express.json());
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
 
+//Making posts
 const posts = [
-  {
-    username: "Patrick",
-    title: "Post 1"
-  },
-  {
-    username: "Prince",
-    title: "Post 2"
-  }
+  { username: "Patrick", title: "Post 1" },
+  { username: "Prince", title: "Post 2" }
 ];
-
+app.use(express.json());
 app.get("/posts", verifyToken, (req, res) => {
-  const user = req.user.name;
-  res.json(posts.filter((post) => post.username === user));
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const user = { name: username };
-  const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "14"
-  });
-  res.json({ acessToken: acessToken });
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "30s"});
+  res.send(token);
 });
 
 function verifyToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader.split(" ")[1];
-  if (token == null) return res.send("There is no token please");
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.send("You are not allowed to use the token");
-    req.user = user;
-    next();
+  if (token == null) return res.send("Token Doesn't exists");
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    if (err) {
+      return res.send("Invalid Token");
+    } else {
+      req.user = data;
+      next();
+    }
   });
 }
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
 });
